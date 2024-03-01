64f7ba1b8464e0a943521a8a0c675a23eb6e43ef
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-03-01 23:10:12 +0530

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,14 +1,27 @@
 // Immediately Invoked Function Expressions (IIFE)
+// (function () {
+//     // …
+//   })();
+  
+//   (() => {
+//     // …
+//   })();
+  
+//   (async () => {
+//     // …
+//   })();
 
+(function sibbu(){   // this is a named IIF
+    console.log(`DB CONNECTED`);
+})();  // this function is invoked but is doesn't know when to stop the context. So, in this context we use semi-colon to stop the context
 
-// (function chai(){
-//     // named IIFE
-//     console.log(`DB CONNECTED`);
-// })();
+(()=>{
+    console.log(`DB CONNECTED TWO`);    
+})();  //again closing the context using semi-colon
 
-// ( (name) => {
-//     console.log(`DB CONNECTED TWO ${name}`);
-// } )('hitesh')
+((name)=>{
+    console.log(`DB CONNECTED$ ${name} TWO`);    
+})('Shahnawaz');
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JS Practice
 A code repo for JavaScript Practice - MDSAK
+
+
+IIFE (Immediately Invoked Function Expression)
+1. An IIFE is a JavaScript function that runs as soon as it is defined.
+2. Avoid polluting the global namespace.
